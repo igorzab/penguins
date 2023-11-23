861ad18203cffd20ffbf4bcf02394c1e8e270f0a
igorzab <igorzabwut@gmail.com>
2023-11-23 12:06:31 +0100

    feat: data type validator

--- main.cpp
+++ main.cpp
@@ -2,18 +2,31 @@
 
 #include "Game.h"
 
+bool validateData(int *x) {
+    while (scanf("%d", x) != 1) {
+        while (getchar() != '\n');
+
+        printf("Invalid input. Please enter an integer: ");
+    }
+}
+
 int main() {
     int numPlayers = 3;
     int size = 15;
     int numPenguins = 1;
 
-//    std::cout << "Plz enter a desired num of players: ";
-//    std::cin >> numPlayers;
-//    std::cout << "\nPlz enter a desired field size: ";
-//    std::cin >> size;
-    int windowSize = size > 20 ? size*20 : size*40;
+    printf("Enter the number of players: ");
+    validateData(&numPlayers);
+
+    printf("Enter the number of penguins: ");
+    validateData(&numPenguins);
+
+    printf("Enter the field size: ");
+    validateData(&size);
+
+    int windowSize = size > 20 ? size * 20 : size * 40;
     sf::RenderWindow window(sf::VideoMode(windowSize, windowSize), "Rybka");
-    play(&window, numPlayers,numPenguins, size);
+    play(&window, numPlayers, numPenguins, size);
 
 
     return 0;
